db3a6c37e5eb2af71afc49ae845c9107a92bdfef
112550119_nycu_sc <163302384+AlanCheng1000@users.noreply.github.com>
2025-12-18 03:19:49 +0800

    Add files via upload

---
+++ demo_final/player.h
@@ -0,0 +1,51 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include <vector>
+#include "hand.h"
+#include "combination.h"
+
+class Player {
+protected:
+    int seatIndex; // Player seat index (0-3)
+    int DB_id;    // Database ID
+    Hand hand;
+    bool isActive;
+public:
+    explicit Player(int seatIndex, int DB_id = -1);
+    virtual ~Player() = default;
+
+    int getseatIndex() const;   // Get player seat index
+    int getDBId() const;        // Get database ID
+    void setDBId(int id);
+
+    Hand& getHand();
+    const Hand& getHand() const;
+    bool isPlayerActive() const;
+
+    void deactivate();
+    void addCard(const Card& card);
+
+    virtual std::vector<Card> playTurn(const Combination& lastPlay, const std::vector<std::vector<Card>>& legalActions, bool canPass) = 0;
+    virtual std::string getTypeName() const = 0;
+    
+    std::vector<Card> playTurnAction(const Combination& lastPlay, const std::vector<std::vector<Card>>& legalActions, bool canPass, const int connfd, char *sendline, char *recvline, bool *is_connected);
+};
+
+class HumanPlayer final : public Player {
+public:
+    explicit HumanPlayer(int seatIndex, int DB_id = -1);
+    
+    std::vector<Card> playTurn(const Combination& lastPlay, const std::vector<std::vector<Card>>& legalActions, bool canPass) override;
+    std::string getTypeName() const override;
+};
+
+class AIPlayer final : public Player {
+public:
+    explicit AIPlayer(int seatIndex, int DB_id = -1);
+    std::vector<Card> playTurn(const Combination& lastPlay, const std::vector<std::vector<Card>>& legalActions, bool canPass) override;
+    std::string getTypeName() const override;
+};
+
+# endif // PLAYER_H
